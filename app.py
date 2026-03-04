552e54dda871b969b304fda2a76ba2411b10203c
MACBOOK <macbook@MACBOOKs-MacBook-Air.local>
2026-03-04 12:15:17 +0100

    Learnt about escape sequence \n \t

---
+++ app.py
@@ -0,0 +1,9 @@
+#.print ("My name is Manny")
+#.print ("Mary is learning python")
+#.print ("Mary wants to become a python Expert")
+
+name =input ("Enter your name:")
+language= input ("prefered language:")
+print ("My name is name")
+print (name is" learning language")
+print (name,"Wants to become a",language ,"Expect")
